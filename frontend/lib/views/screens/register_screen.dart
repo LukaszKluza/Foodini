f872268d84eab94474242ea7cad519d11ed50753
Szymon Janik <szymonjanik2003@gmail.com>
2025-03-25 01:57:30 +0100

    Basic registration data validation

--- frontend/lib/views/screens/register_screen.dart
+++ frontend/lib/views/screens/register_screen.dart
@@ -10,6 +10,7 @@ class RegisterScreen extends StatefulWidget {
 
 class _RegisterScreenState extends State<RegisterScreen> {
   final _formKey = GlobalKey<FormState>();
+  final TextEditingController _passwordController = TextEditingController();
 
   void _register() {
     if (_formKey.currentState!.validate()) {
@@ -17,6 +18,50 @@ class _RegisterScreenState extends State<RegisterScreen> {
         context,
       ).showSnackBar(SnackBar(content: Text('Registered successfully')));
     }
+  }
+
+  String? _validateFirstname(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'First name is required';
+    }
+    return null;
+  }
+
+  String? _validateLastname(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Last name is required';
+    }
+    return null;
+  }
+
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'E-mail is required';
+    }
+    if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+      return 'Enter valid e-mail';
+    }
+    return null;
+  }
+
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Password is required';
+    }
+    if (value.length < 8) {
+      return 'Password must have at least 8 characters';
+    }
+    return null;
+  }
+
+  String? _validateConfirmPassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Password confirmation is required';
+    }
+    if (value != _passwordController.text) {
+      return 'Passwords must be the same';
+    }
+    return null;
   }
 
   @override
@@ -30,32 +75,49 @@ class _RegisterScreenState extends State<RegisterScreen> {
           ),
         ),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(labelText: 'E-mail'),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Reenter password'),
-                obscureText: true,
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(onPressed: _register, child: Text('Register')),
-              TextButton(
-                onPressed: () => context.go('/login'),
-                child: Text('Already have an account? Login'),
-              ),
-              TextButton(onPressed: () => context.go('/'), child: Text('Home'))
-          ])),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(35.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'First name'),
+                    validator: _validateFirstname,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Last name'),
+                    validator: _validateLastname,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'E-mail'),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: _validateEmail,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    controller: _passwordController,
+                    validator: _validatePassword,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Confirm password'),
+                    obscureText: true,
+                    validator: _validateConfirmPassword,
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(onPressed: _register, child: Text('Register')),
+                  TextButton(
+                    onPressed: () => context.go('/login'),
+                    child: Text('Already have an account? Login'),
+                  ),
+                  TextButton(onPressed: () => context.go('/'), child: Text('Home'))
+              ])),
+          ),
+        ],
       ),
     );
   }
