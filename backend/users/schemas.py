ea4662c4fba24147cf9dcf3d56bab5d1df155b3b
Mateusz <zsuetam36@gmail.com>
2025-04-01 19:17:19 +0200

    Change in Filed from pydantic regex -> pattern

--- backend/users/schemas.py
+++ backend/users/schemas.py
@@ -4,8 +4,8 @@ import re
 
 
 class UserCreate(BaseModel):
-    name: str = Field(..., min_length=2, max_length=50, regex="^[a-zA-Z]+$")
-    last_name: str = Field(..., min_length=2, max_length=50, regex="^[a-zA-Z-]+$")
+    name: str = Field(..., min_length=2, max_length=50, pattern="^[a-zA-Z]+$")
+    last_name: str = Field(..., min_length=2, max_length=50, pattern="^[a-zA-Z-]+$")
     age: int = Field(..., gt=12, lt=120)
     country: str = Field(..., min_length=2, max_length=50)
     email: EmailStr
@@ -24,9 +24,11 @@ class UserCreate(BaseModel):
 
 class UserUpdate(BaseModel):
     user_id: int = Field(..., gt=0)
-    name: Optional[str] = Field(None, min_length=2, max_length=50, regex="^[a-zA-Z]+$")
+    name: Optional[str] = Field(
+        None, min_length=2, max_length=50, pattern="^[a-zA-Z]+$"
+    )
     last_name: Optional[str] = Field(
-        None, min_length=2, max_length=50, regex="^[a-zA-Z-]+$"
+        None, min_length=2, max_length=50, pattern="^[a-zA-Z-]+$"
     )
     age: Optional[int] = Field(None, gt=12, lt=120)
     country: Optional[str] = Field(None, min_length=2, max_length=50)
