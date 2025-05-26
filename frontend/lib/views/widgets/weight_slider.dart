39d24ae91d137179bdf326d34bd0d8bba9f47a39
Lukasz <lukasz.9@o2.pl>
2025-05-26 18:04:04 +0200

    Update after review v2

--- frontend/lib/views/widgets/weight_slider.dart
+++ frontend/lib/views/widgets/weight_slider.dart
@@ -86,7 +86,6 @@ class WeightSliderState extends State<WeightSlider> {
           ),
         ),
         GestureDetector(
-          // onTap: _showWeightDialog,
           child: Slider(
             value: _weight,
             min: widget.min,
