438c02ac30dfbc785e5cbe7945870c77e02966eb
Szymon Janik <szymonjanik2003@gmail.com>
2025-09-13 22:06:01 +0200

    Of course I forgot about ruff

--- backend/user_details/mixins/diet_goal_validation_mixin.py
+++ backend/user_details/mixins/diet_goal_validation_mixin.py
@@ -8,7 +8,9 @@ class DietGoalValidationMixin:
     @model_validator(mode="after")
     def check_diet_goal(cls, values):
         if values.diet_type == DietType.FAT_LOSS and values.diet_goal_kg >= values.weight_kg:
-            raise ValueErrorException("Diet goal can't be greater than weight for diet type FAT LOSS.",)
+            raise ValueErrorException(
+                "Diet goal can't be greater than weight for diet type FAT LOSS.",
+            )
         elif values.diet_type == DietType.MUSCLE_GAIN and values.diet_goal_kg <= values.weight_kg:
             raise ValueErrorException("Diet goal can't be lower than weight for diet type MUSCLE GAIN.")
         elif values.diet_type == DietType.WEIGHT_MAINTENANCE and values.diet_goal_kg != values.weight_kg:
