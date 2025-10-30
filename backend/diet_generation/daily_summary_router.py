e7a72e986fdf221bb145ad50aeebd9a11cabbba6
Lukasz <lukasz.9@o2.pl>
2025-10-30 21:57:49 +0100

    #183-fetch-last-generated-meals. Cleanup

--- backend/diet_generation/daily_summary_router.py
+++ backend/diet_generation/daily_summary_router.py
@@ -3,7 +3,7 @@ from datetime import date
 from fastapi import APIRouter, Depends, status
 
 from backend.diet_generation.daily_summary_service import DailySummaryService
-from backend.diet_generation.dependencies import get_daily_summary_service, get_last_generated_meals_repository
+from backend.diet_generation.dependencies import get_daily_summary_service
 from backend.diet_generation.schemas import (
     CustomMealUpdateRequest,
     DailyMacrosSummaryCreate,
@@ -95,23 +95,3 @@ async def get_meal_details(
 ):
     user, _ = await user_gateway.get_current_user()
     return await daily_summary_service.get_meal_details(meal_id)
-
-
-# REMOVE AFTER TESTS
-from backend.diet_generation.last_generated_meals_repository import LastGeneratedMealsRepository
-import uuid
-from typing import List
-
-
-@daily_summary_router.get("/daily-summary/macros/{from_day}/{to_day}", response_model=List[str])
-async def get_daily_macros_summary(
-    from_day: date,
-    to_day: date,
-    last_generated_meals_repository: LastGeneratedMealsRepository = Depends(get_last_generated_meals_repository),
-    # user_gateway: UserGateway = Depends(get_user_gateway),
-):
-    # user, _ = await user_gateway.get_current_user()
-
-    uuid_value = uuid.UUID("e312ee7c-5753-40ef-b438-183aa4d01d66")
-
-    return await last_generated_meals_repository.get_last_generated_meals(uuid_value, from_day, to_day)
--- backend/diet_generation/last_generated_meals_repository.py
+++ backend/diet_generation/last_generated_meals_repository.py
@@ -4,7 +4,7 @@ from typing import List
 from sqlalchemy import UUID, select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-from backend.models import Meal, MealRecipe
+from backend.models import MealRecipe
 from backend.models.user_daily_summary_model import DailyMealsSummary, MealToDailySummary
 
 
@@ -15,29 +15,6 @@ class LastGeneratedMealsRepository:
     async def get_last_generated_meals(self, user_id: UUID, from_date: date, to_date: date) -> List[str]:
         dms = DailyMealsSummary
         mt = MealToDailySummary
-        m = Meal
-        mr = MealRecipe
-
-        query = (
-            select(mr.meal_name)
-            .select_from(dms)
-            .join(mt, mt.daily_summary_id == dms.id)
-            .join(m, m.id == mt.meal_id)
-            .join(mr, mr.meal_id == m.id)
-            .where(
-                dms.user_id == user_id,
-                dms.day >= from_date,
-                dms.day <= to_date
-            )
-            .distinct()
-        )
-
-        result = await self.db.execute(query)
-        return result.scalars().all()
-
-    async def get_last_generated_meals1(self, user_id: UUID, from_date: date, to_date: date) -> List[str]:
-        dms = DailyMealsSummary
-        mt = MealToDailySummary
         mr = MealRecipe
 
         query = (
@@ -45,11 +22,7 @@ class LastGeneratedMealsRepository:
             .select_from(dms)
             .join(mt, mt.daily_summary_id == dms.id)
             .join(mr, mr.meal_id == mt.meal_id)
-            .where(
-                dms.user_id == user_id,
-                dms.day >= from_date,
-                dms.day <= to_date
-            )
+            .where(dms.user_id == user_id, dms.day >= from_date, dms.day <= to_date)
             .distinct()
         )
 
