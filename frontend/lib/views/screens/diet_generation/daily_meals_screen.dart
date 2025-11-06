c736b6cf30136e4f6a7113e9146aca3ffa0f17b9
Mateusz Sacha <zsuetam36@gmail.com>
2025-11-06 01:47:00 +0100

    Mocs and tests

--- frontend/lib/views/screens/diet_generation/daily_meals_screen.dart
+++ frontend/lib/views/screens/diet_generation/daily_meals_screen.dart
@@ -6,7 +6,6 @@ import 'package:frontend/events/diet_generation/daily_summary_events.dart';
 import 'package:frontend/foodini.dart';
 import 'package:frontend/models/user/language.dart';
 import 'package:frontend/states/diet_generation/daily_summary_states.dart';
-import 'package:frontend/views/widgets/action_button.dart';
 import 'package:frontend/views/widgets/bottom_nav_bar.dart';
 import 'package:frontend/views/widgets/generate_meals_button.dart';
 import 'package:go_router/go_router.dart';
